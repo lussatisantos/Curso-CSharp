0a3b7ea1a1ec6faa685a3577e96dde3585748c4e
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-16 17:28:03 +0000

    variaveis

---
+++ 03/aula3.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Aula3{
+    static void Main() {
+        int num = 0;
+        char letra = 'l';
+        float valor = 0.4f;
+        byte n1 = 10;
+        string nome = "lussati";
+
+
+        var aux = nome;
+
+        Console.WriteLine("Valor da variavel: " + aux + "...");
+    }
+}
