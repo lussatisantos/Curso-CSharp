ac7770793a18ffb65319c3c0db18c93269747685
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-20 15:15:17 +0000

    escopo de variaveis

---
+++ 04/aula04.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Aula04 {
+
+    static int num = 10;
+
+    static void Main() {
+        int num2;
+        
+        if (num == 10) {
+            num = 0;
+        }
+
+        Console.WriteLine(num);
+    }
+}
