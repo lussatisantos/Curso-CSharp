7018351f6b163c234a3acefd9696c830544c5e82
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-21 12:42:48 +0000

    constantes

---
+++ 05/aula05.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Aula05 {
+    static void Main(){
+        const string nome = "Lussati Santos";
+        const double numb = 100.3221;
+
+        Console.WriteLine(nome);
+        Console.WriteLine("{0} escreveu {1} de forma aleatoria", nome, numb);
+    }
+}
