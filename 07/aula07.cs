c84dedb311ae1cb8ae4ea1284465ffd8f3c22932
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-21 13:14:51 +0000

    bitwise

---
+++ 07/aula07.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula07 {
+    static void Main() {
+
+        // << bitwise que dobra o valor
+        // >> bitwise que diminue pela metade o valor
+
+        int num = 2;
+
+        num = num<<1;
+
+        Console.WriteLine(num); 
+    }
+}
