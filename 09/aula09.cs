ca23ee834e1f8734ac9225e37e80ad0d4582bd4b
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-23 17:56:37 +0000

    typecast

---
+++ 09/aula09.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Aula09 {
+    static void Main() {
+
+        // typescast
+
+        /*
+        float n2 = 10.9f;
+        int n1 =(int)n2; // operacao de type cast
+
+        Console.WriteLine(n1); 
+        */
+
+        int vInt = 10;
+        short vShort = (short) vInt;
+
+        Console.WriteLine(vShort);
+    }
+}
